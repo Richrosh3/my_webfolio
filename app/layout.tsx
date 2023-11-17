90f433fb016e32fc3fcd5b684d806dd70fb18712
Roshen Abraham <richrosh3@gmail.com>
2023-11-17 17:21:38 -0500

    Initial commit for my_webfolio

---
+++ app/layout.tsx
@@ -0,0 +1,24 @@
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import './globals.css'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: "Roshen's Webfolio",
+  description: "Roshen is a junior software engineer eager to learn different branches of CS!",
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        {children}
+      </body>
+    </html>
+  )
+}
