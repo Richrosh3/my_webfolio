ce37b9ffd68bcee6bb5a35e13fb0632a5b29df95
Roshen Abraham <richrosh3@gmail.com>
2023-11-18 19:47:47 -0500

    Completed Intro Section

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} bg-slate-300 text-gray-950 relative h-[5000px]`}>
+      <body className={`${inter.className} bg-slate-300 text-gray-950 relative h-[5000px] pt-28 sm:pt-36`}>
         <div className='bg-[#fbe2e3] absolute top-[-6rem] right-[11rem] -z-10
         h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem]'/>
         <div className='bg-[#dbd7fb] absolute top-[-1rem] left-[-35rem] -z-10
