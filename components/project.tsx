3f90869322b059eb0ce3907dbb358ca7c11c10ca
Roshen Abraham <richrosh3@gmail.com>
2023-11-19 14:06:50 -0500

    Implemented Projects section

---
+++ components/project.tsx
@@ -0,0 +1,66 @@
+"use client";
+
+import { useRef } from "react";
+import { projectsData } from "@/lib/data";
+import Image from "next/image";
+import { motion, useScroll, useTransform } from "framer-motion";
+
+type ProjectProps = (typeof projectsData)[number];
+
+export default function Project({
+  title,
+  description,
+  tags,
+  imageUrl,
+}: ProjectProps) {
+  const ref = useRef<HTMLDivElement>(null);
+  const { scrollYProgress } = useScroll({
+    target: ref,
+    offset: ["0 1", "1.33 1"],
+  });
+
+  const scaleProgress = useTransform(scrollYProgress, [0, 1], [.6, 1]);
+  const opacityProgress = useTransform(scrollYProgress, [0, 1], [.4, 1]);
+  
+
+  return (
+    <motion.div
+        style={{
+            scale: scaleProgress,
+            opacity: opacityProgress,
+        }}
+        className = "group mb-3 sm:mb-8 last:mb-0"
+    >
+      <section
+        ref={ref}
+        className="bg-blue-50 max-w-[42rem] border border-black/5 overflow-hidden sm:pr-8 relative sm:h-[20rem] group-even:pl-8 hover:bg-sky-100 transition rounded-lg"
+      >
+        <div className="pt-4 pb-7 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] flex flex-col h-full group-even:ml-[18rem]">
+          <h3 className="text-2xl font-semibold">{title}</h3>
+          <p className="mt-2 leading-relaxed text-gray-700">{description}</p>
+          <ul className="flex flex-wrap mt-4 gap-2 sm:mt-auto">
+            {tags.map((tag, index) => (
+              <li
+                className="bg-sky-500 px-3 py-1 text-[0.7rem] uppercase tracking-wider text-white rounded-full"
+                key={index}
+              >
+                {tag}
+              </li>
+            ))}
+          </ul>
+        </div>
+
+        <Image
+          alt={title}
+          src={imageUrl}
+          quality={100}
+          className="
+            absolute top-8 -right-40 w-[28.25rem] rounded-t-lg shadow-2xl 
+            transition group-hover:scale-[1.04] group-hover:-translate-x-3 group-hover:translate-y-3 group-hover:-rotate-2 
+            group-even:right-[initial] group-even:-left-40 group-even:group-hover:translate-x-3 group-even:group-hover:translate-y-3 group-even:group-hover:rotate-2
+        "
+        />
+      </section>
+    </motion.div>
+  );
+}
